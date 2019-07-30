62d11e418715c8f7d0851cecdbe286ec77a0a2e8
madvirus <madvirus@madvirus.net>
2019-07-30 17:57:25 +0900

    Initial commit

---
+++ src/main/java/rangeable/SpringDataJpaRangeableApplication.java
@@ -0,0 +1,13 @@
+package rangeable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaRangeableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaRangeableApplication.class, args);
+    }
+
+}
